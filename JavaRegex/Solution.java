a85fd0470b9b9ae4691f35cd038c7afbc6d9c38a
«M.R.» <rubshikmax@gmail.com>
2019-06-24 18:37:26 +0300

    upd

--- JavaRegex/Solution.java
+++ JavaRegex/Solution.java
@@ -1,29 +1,24 @@
 package JavaRegex;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while(in.hasNext()){
+        while (in.hasNext()) {
             String IP = in.next();
             System.out.println(IP.matches(new MyRegex().pattern));
         }
 
     }
-    class Regex {
-        String IPADDRESS_PATTERN =
-                "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.)" +
-                        "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
-        Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
-        Matcher matcher = pattern.matcher(IPADDRESS_PATTERN);
-        if(matcher.find())
-
-        {
-            return true;
-        }
-    }
 }
+
+class MyRegex {
+    public String pattern =
+            "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.)" +
+                    "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+    
+}
+
