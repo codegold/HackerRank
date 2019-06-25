7d786561e2cec3b29dc5b2c9aa75cb1d3662cd3f
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-06-25 14:49:08 +0300

    Add files via upload

---
+++ JavaRegex/Solution.java
@@ -0,0 +1,24 @@
+package JavaRegex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+    public String pattern =
+            "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.)" +
+                    "{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+
+}
+
