a0421edbc5c915fbec8a4d079841f6cde287c6e6
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-07-08 22:28:27 +0300

    Add files via upload

---
+++ JavaList/Solution.java
@@ -0,0 +1,30 @@
+package JavaList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        //Create and fill in
+        List<Integer> myL = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            int val = scanner.nextInt();
+            myL.add(val);
+        }
+
+        //Make queries
+        int Q = scanner.nextInt();
+        for (int i = 0; i < Q ; i++) {
+            String command = scanner.next();
+            if(command.equals("Insert")) {
+                int index = scanner.nextInt();
+                int value = scanner.nextInt();
+                myL.add(index, value);
+            }
+        }
+    }
+}
