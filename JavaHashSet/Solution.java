a19d0a36606ce22c4425de161998fb9812c83ade
«M.R.» <rubshikmax@gmail.com>
2019-07-08 23:58:21 +0300

    upd

---
+++ JavaHashSet/Solution.java
@@ -0,0 +1,25 @@
+package JavaHashSet;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+        //Write your code here
+        HashSet<String> myHashSet = new HashSet<>(t);
+        for (int i = 0; i < t; i++) {
+            myHashSet.add("(" + pair_left[i] + ", " + pair_right[i] + ")" );
+            System.out.println(myHashSet.size());
+        }
+
+    }
+}
