dcde71f450cbff8a14d0c5370cd8614e49f0256c
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-07-24 15:39:52 +0300

    Add files via upload

---
+++ DAY13AbstractClasses/Solution.java
@@ -0,0 +1,64 @@
+package DAY13AbstractClasses;
+
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+// Declare your class here. Do not use the 'public' access modifier.
+// Declare the price instance variable
+class MyBook extends Book {
+    int price;
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    @Override
+    void display() {
+        System.out.println("Title: " + this.title);
+        System.out.println("Author: " + this.author);
+        System.out.println("Price: " + this.price);
+    }
+}
+
+/**
+ * Class Constructor
+ *
+ * @param title The book's title.
+ * @param author The book's author.
+ * @param price The book's price.
+ **/
+// Write your constructor here
+
+/**
+ * Method Name: display
+ * <p>
+ * Print the title, author, and price in the specified format.
+ **/
+// Write your method here
+
+// End class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
