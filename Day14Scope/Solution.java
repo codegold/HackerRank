8004f036215a5b7e5f23f7fab8c2d4855ad0204e
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-07-24 15:39:26 +0300

    Add files via upload

---
+++ Day14Scope/Solution.java
@@ -0,0 +1,53 @@
+package Day14Scope;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+
+    Difference(int[] a) {
+        elements = a;
+    }
+
+    void computeDifference() {
+        maximumDifference = 0;
+        for (int i = 0; i < elements.length - 1; i++) {
+            for (int j = i + 1; j < elements.length-1; j++) {
+                //abs = for escape possible minus result.
+                int temp = Math.abs(elements[i] - elements[j]);
+                if (temp > maximumDifference) {
+                    maximumDifference = temp;
+                }
+
+            }
+        }
+    }
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
